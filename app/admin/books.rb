d9d67e303be2a84d1642cc2f061b13f918c010ca
Raghu Betina <raghu@firstdraft.com>
2019-12-04 22:41:37 +0000

    Whitelist columns in Book for ActiveAdmin

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Book do
+
+ permit_params :title, :author, :year, :subject, :pages, :editorial, :edition
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
